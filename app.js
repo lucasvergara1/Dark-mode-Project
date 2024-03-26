f9f69f9f43fff48cafdfaad7fc9d191beefedbdf
Lucas <lucas.vergara@live.com>
2024-03-25 23:57:59 -0300

    :construction: working in progress

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles')
 
 toggleBtn.addEventListener('click', ()=>{
     document.documentElement.classList.toggle('dark-theme');
+});
+
+const articlesData = articles.map(()=>{
+const {title, date, length, snippet} = article
+//format date
+
+return `<article class="post">
+        <h2> ${title}</h2>
+        <div class="post-info">
+        <span>${date}</span>
+        <span>${length} min read</span>
+        </div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Voluptates rerum deleniti illum perspiciatis vel, eaque 
+          recusandae delectus excepturi placeat accusamus, 
+          repellat veniam explicabo tempore earum alias sit 
+          numquam quae unde.</p>
+
+      </article>`;
+
 });
 
