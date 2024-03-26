9cf55314eea5ecdb9b7ee972e420745be69728c0
Lucas <lucas.vergara@live.com>
2024-03-26 00:20:37 -0300

    End of the project

--- app.js
+++ app.js
@@ -5,23 +5,25 @@ toggleBtn.addEventListener('click', ()=>{
     document.documentElement.classList.toggle('dark-theme');
 });
 
-const articlesData = articles.map(()=>{
-const {title, date, length, snippet} = article
+const articlesData = articles.map((article)=>{
+const {title, date, length, snippet} = article;
 //format date
+
+const formatDate = moment(date).format('MMMM Do, YYYY');
 
 return `<article class="post">
         <h2> ${title}</h2>
         <div class="post-info">
-        <span>${date}</span>
+        <span>${formatDate}</span>
         <span>${length} min read</span>
         </div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-          Voluptates rerum deleniti illum perspiciatis vel, eaque 
-          recusandae delectus excepturi placeat accusamus, 
-          repellat veniam explicabo tempore earum alias sit 
-          numquam quae unde.</p>
-
+        <p>${snippet}</p>
       </article>`;
 
-});
+})
+.join('');
+
+
+articlesContainer.innerHTML = articlesData;
+
 
